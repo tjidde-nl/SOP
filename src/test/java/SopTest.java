c431385d5106c4ff693dc4ecc4392340b35d80d9
tjidde-nl <tjidde-nl@users.noreply.github.com>
2019-06-11 23:57:26 +0200

    addes unittest

---
+++ src/test/java/SopTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert .assertEquals;
+public class SopTest {
+    @Test
+    public void testCanViewHelloWorld() {
+        String test = "SOP test!";
+        assertEquals("SOP test!", test);
+    }
+}
